b98820c584d3aa39d9da7020c7d3558bc1cf3435
AHAMAD NAYEM MOZUMDER <mozahmad22@gmail.com>
2023-05-29 23:04:13 +0600

    Add files via upload

---
+++ Controller/loginController.php
@@ -0,0 +1,18 @@
+<?php
+  session_start();
+  include('controller.php');
+
+  if (isset($_POST['submit'])) {
+    $_SESSION['username'] = $_POST['username'];
+
+    $result = $model->login();
+
+    if($result['PASSWORD'] == $_POST['password']){
+        $_SESSION['STID'] = $result['STID'];
+        echo "<script type='text/javascript'>
+                  window.location='../View/unfinished.php';
+              </script>";
+      exit();
+    } else echo '<script type ="text/Javascript">alert("NOT A STUDENT OF THIS UNIVERSITY");</script>';
+  }
+
