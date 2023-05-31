14d1442fbf7ae169564c4f6a71f25fbf8f019926
AHAMAD NAYEM MOZUMDER <mozahmad22@gmail.com>
2023-05-31 22:00:22 +0600

    Delete Model directory

--- Model/connect.php
+++
@@ -1,8 +0,0 @@
-<?php  
-	$con= new mysqli("localhost","id20762354_nayemahmad","Ojbfe78?.","id20762354_student_management_system");
-
-	//check connection
-	if(!$con){
-		die("Connection failed: ".$con-> connect_error);
-	}
-?>	
