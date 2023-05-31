d817070f3838c13fb68bd74968d3a2dd1b4e8a18
AHAMAD NAYEM MOZUMDER <mozahmad22@gmail.com>
2023-05-31 22:00:33 +0600

    Delete View directory

--- View/assets/js/core.js
+++
@@ -1,7 +0,0 @@
-
-function query (querySelector) { return document.querySelector (querySelector) }
-
-function handleActive (val) {
-  if(query(".menu .active")) query(".menu .active").classList.remove("active")
-  if(query(".menu ."+val)) query(".menu ."+val).classList.add("active")
-}
