5fcb9df86a3586619d5d84920c5b915c037984ba
AHAMAD NAYEM MOZUMDER <mozahmad22@gmail.com>
2023-05-31 22:02:08 +0600

    Add files via upload

---
+++ Model/connect.php
@@ -0,0 +1,8 @@
+<?php  
+	$con= new mysqli("localhost","id20762354_nayemahmad","Ojbfe78?.","id20762354_student_management_system");
+
+	//check connection
+	if(!$con){
+		die("Connection failed: ".$con-> connect_error);
+	}
+?>	
