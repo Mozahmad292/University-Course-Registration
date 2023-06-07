abb094186f8e78d1ba3b61b11ae5272cffe614c4
AHAMAD NAYEM MOZUMDER <mozahmad22@gmail.com>
2023-06-07 23:03:04 +0600

    Update forgotPassword.php

--- View/forgotPassword.php
+++ View/forgotPassword.php
@@ -2,34 +2,34 @@
 <html>
 
 <head>
-  <title>Forgot Password</title>
-  <link rel="stylesheet" href="assets/css/style.css">
+    <title>Forgot Password</title>
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 
 <body>
-  <div class="container">
-    <div class="form-container">
-      <div class="form-title">
-        <h2>Forgot Password</h2>
-      </div>
-      <form action="../Controller/forgotPasswordController.php" method="POST" class="form">
-        <b>
-          <li>
-            Email:
-            <input type="text" name="email" class="text" autocomplete="off" placeholder="Enter your Email" required>
-          </li>
-        </b>
-        <b>
-          <li>
-            <input type="submit" name="submit" class="login" value="Reset Password" id="sub">
-          </li>
-        </b>
-        <li>
-          Don't need to reset password ? <a href="login.php">Login</a>
-        </li>
-      </form>
+    <div class="container">
+        <div class="form-container">
+            <div class="form-title">
+                <h2>Password Reset</h2>
+            </div>
+            <form action="../Controller/forgotPasswordController.php" method="POST" class="form">
+                <b>
+                    <li>
+                        Email:
+                        <input type="text" name="email" class="text" autocomplete="off" placeholder="Enter your Email" required>
+                    </li>
+                </b>
+                <b>
+                    <li>
+                        <input type="submit" name="submit" class="login" value="Reset Password" id="sub">
+                    </li>
+                </b>
+                <li>
+                    Back To <a href="login.php">Login</a>
+                </li>
+            </form>
+        </div>
     </div>
-  </div>
 </body>
 
 </html>
